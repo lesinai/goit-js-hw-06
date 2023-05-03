820e0e7cf406dcdddd4a1535541fb033d8548185
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-03 22:14:01 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listRef = document.querySelector('#ingredients');
+
+const itemsToAdd = ingredients.map(item => {
+  const readyItem = document.createElement('li');
+  readyItem.classList.add('item');
+  readyItem.textContent = item;
+  return readyItem;
+})
+
+listRef.append(...itemsToAdd);
