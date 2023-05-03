126f13882984f41939365f66c8e34c0256902fd3
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-03 20:30:28 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,11 @@
-const
+const commonListRef = document.querySelector('#categories');
+const commonListItemRef = [...commonListRef.children];
+
+console.log(`Number of categories: ${commonListRef.children.length}`);
+console.log('\n');
+
+commonListItemRef.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
+    console.log('\n');
+});
