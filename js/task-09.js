83575efac3401e10c218be90edf7191d806ebb2d
lesinai <90961075+lesinai@users.noreply.github.com>
2023-05-04 08:23:58 +0200

    upd 9-10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonRef = document.querySelector('.change-color');
+const textRef = document.querySelector('.color');
+
+function onButtonRandomize () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  textRef.textContent = randomColor;
+}
+buttonRef.addEventListener('click', onButtonRandomize);
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtnRef = document.querySelector('button[data-create]');
+const destroyBtnRef = document.querySelector('button[data-destroy]');
+const inputRef = createBtnRef.previousElementSibling;
+const targetDiv = document.querySelector('#boxes');
+
+function stringMaker() {
+  let size = 30;
+  let string = '';
+
+  for (let i = 1; i <= inputRef.value; i++) {
+    let color = getRandomHexColor();
+
+    string += `<div style="background-color:${color}; width:${size}px; height:${size}px;"></div>`;
+    size += 10;
+  }
+  return string
+}
+
+createBtnRef.addEventListener('click', () => targetDiv.insertAdjacentHTML("beforeend", stringMaker()) );
+destroyBtnRef.addEventListener('click', () => targetDiv.innerHTML = '')
